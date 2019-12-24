a6eb9aed347c738fdf65b94f569ff0a4a70a30da
Trần Hồng Ngọc <ngocth@vissoft.vn>
2019-12-24 18:30:00 +0700

    create module front-end

--- datngoc-backend/src/main/java/com/project/datngoc/repo/impl/Model1RepoImpl.java
+++ datngoc-backend/src/main/java/com/project/datngoc/repo/impl/Model1RepoImpl.java
@@ -6,6 +6,7 @@ import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import java.util.List;
@@ -26,11 +27,17 @@ public class Model1RepoImpl implements Model1Repo {
 
     @Override
     public Object[] getById(Integer id) throws Exception {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("select * from table_test m where m.id= :modelId");
-        Query query = entityManager.createNativeQuery(stringBuilder.toString());
-        query.setParameter("modelId", id);
-        return (Object[]) query.getSingleResult();
+        try{
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("select * from table_test m where m.id= :modelId");
+            Query query = entityManager.createNativeQuery(stringBuilder.toString());
+            query.setParameter("modelId", id);
+            return (Object[]) query.getSingleResult();
+        } catch (NoResultException no) {
+                no.printStackTrace();
+            return null;
+        }
+
     }
 
 
